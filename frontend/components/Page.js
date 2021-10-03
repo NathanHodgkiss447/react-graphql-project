d963941c47b896087a1353a9c269822de56fa6f1
NathanHodgkiss447 <nathanhodgkiss447@gmail.com>
2021-10-03 17:42:52 +0100

    Added global styles

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,9 +1,25 @@
 import PropTypes from 'prop-types';
+import { createGlobalStyle } from 'styled-components';
 import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0px 12px 24px 0 rgba(0,0,0,0.09);
+  }
+`;
 
 export default function Page({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Header> </Header>
       <h1>I am the page component</h1>
       {children}

8bed660e0316252c18871780aaf74fa3d0685e50
NathanHodgkiss447 <nathanhodgkiss447@gmail.com>
2021-10-03 16:54:52 +0100

    Fixed merge conflicts

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
-<<<<<<< HEAD
-import Page from '../components/Page.js';
-=======
 import Page from '../components/Page';
->>>>>>> 5676ec4fc3df97de5327d64362e60c942e6b7167
 
 export default function MyApp({ Component, pageProps }) {
   return (
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,11 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-GB">
-<<<<<<< HEAD
-        {/* <Head></Head> */}
-=======
         {/* Head */}
->>>>>>> 5676ec4fc3df97de5327d64362e60c942e6b7167
         <body>
           <Main />
           <NextScript />

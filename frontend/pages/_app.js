ae701994588bdc7cbdec01763544d1a5408b9799
NathanHodgkiss447 <nathanhodgkiss447@gmail.com>
2022-02-20 16:23:47 +0000

    setting up apollo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,35 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
+import { ApolloProvider } from '@apollo/client';
+import { Router } from 'next/dist/client/router';
+import nProgress from 'nprogress';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
-export default function MyApp({ Component, pageProps }) {
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
+
+function MyApp({ Component, pageProps, apollo }) {
+  console.log(apollo);
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+
+  return { pageProps };
+};
+
+export default withData(MyApp);
